edb39fba3d8026da1ac0630bc2ec4c3318f0977a
Hugo <func.hugo@gmail.com>
2018-11-23 23:10:00 +0000

    First commit

---
+++ src/components/EmployeeList.js
@@ -0,0 +1,30 @@
+import _ from 'lodash'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { FlatList } from 'react-native'
+import { employeesFetch } from '../actions'
+import ListItem from './ListItem'
+
+class EmployeeList extends Component {
+  componentDidMount() {
+    const { employeesFetch } = this.props
+    employeesFetch()
+  }
+
+  render() {
+    return (
+      <FlatList
+        data={this.props.employees}
+        renderItem={employee => <ListItem employee={employee.item} />}
+        keyExtractor={employee => employee.uid}
+      />
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  const employees = _.map(state.employees, (values, uid) => ({ ...values, uid }))
+  return { employees }
+}
+
+export default connect(mapStateToProps, { employeesFetch })(EmployeeList)
